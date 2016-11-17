f8807ea145bfb23d61036370c2ad15b29dda89d9
jrnorton11 <jrnorton0@gmail.com>
2016-11-16 22:15:02 -0800

    Programming Assignment #2 - Jeffrey Norton

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is expensive.  This set of functions holds a
+## square, (assumed) invertible matrix.  It uses lazy evaluation
+## such that the inverse of the matrix is stored in the object
+## after being calculated the first time.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix
+## Create an object with a valid square matrix that can be inverted
+## (not checking for singularities or degeneracies)
+## If the object cannot be coerced to a proper square matrix, the
+## function will fail fatally.
+##
+makeCacheMatrix <- function(m = matrix()) {
+  # Check that m is a matrix
+  if(class(m) != "matrix") {
+    # Coerce to a matrix if possible
+    dim <- sqrt(length(m))
+    if( trunc(dim)-dim != 0 ) {
+      stop("Cannot invert non-square matrix after coersion")
+    }
+    # Generate the matrix.
+    m <- matrix(data=as.numeric(m),
+    nrow=as.integer(dim), ncol=as.integer(dim))
+  } else {
+    if( dim(m)[1] != dim(m)[2] ) {
+      stop("Cannot invert non-square matrix")
+    }
+  }
+  # Return the cacheMatrix.  We fill in the inverse now just
+  # for "cleanliness" of the inversion code.
+  return(list("matrix"=m, "inverse"=NULL))
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve
+# Return the inverted matrix if cached.  If not, calculate it,
+# then return it.
+cacheSolve <- function(cacheMatrix, ...) {
+  if( is.null(cacheMatrix$inverse) ) {
+    # Calculate the previously uncalculated inverted matrix.
+    cacheMatrix$inverse <- solve(cacheMatrix$matrix)
+  }
+  cacheMatrix
 }
+
